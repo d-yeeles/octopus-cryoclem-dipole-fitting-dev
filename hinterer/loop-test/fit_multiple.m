16e464ebd72e3c4ce1857e475925ecdc845f6a74
Dave Yeeles <dave@yeel.es>
2024-11-15 13:46:36 +0000

    updated progress on hinterer loop

--- hinterer/loop-test/fit_multiple.m
+++ hinterer/loop-test/fit_multiple.m
@@ -3,7 +3,7 @@
 close all
 clear all
 
-addpath(genpath('./'));
+addpath(genpath('../'));
 
 % Define base parameters
 %baseParams.nPixels = 171;
@@ -11,11 +11,11 @@ addpath(genpath('./'));
 %baseParams.defocusRange = [-500, 500];  % Defocus range
 
 % Input params
-hinterer_results_path = '/home/tfq96423/Documents/cryoCLEM/dipole-issue/localizationFixedDipoles-motionFit/loop-test/hinterer_results.csv';
-thunderstorm_results_path = '/home/tfq96423/Documents/cryoCLEM/dipole-issue/localizationFixedDipoles-motionFit/loop-test/thunderstorm_results.csv';
+hinterer_results_path = '/home/tfq96423/Documents/cryoCLEM/dipole-issue/fixed-dipole-issue/hinterer/loop-test/hinterer_results.csv';
+thunderstorm_results_path = '/home/tfq96423/Documents/cryoCLEM/dipole-issue/fixed-dipole-issue/hinterer/loop-test/thunderstorm_results.csv';
+frames_dir = '/home/tfq96423/Documents/cryoCLEM/dipole-issue/fixed-dipole-issue/hinterer/loop-test/ASIL240923C05_50.ome.tif-frames/';
 pixel_width = 51.2; % Pixel width (nm per px)
-frames_dir = '/home/tfq96423/Documents/cryoCLEM/dipole-issue/localizationFixedDipoles-motionFit/loop-test/ASIL240923C05_50.ome.tif-frames/';
-patch_width = 12; % size of NxN pixel patch around blob centroid to consider
+patch_width = 13; % size of NxN pixel patch around blob centroid to consider (must be odd)
 
 
 % Run thunderstorm on stack
@@ -101,7 +101,8 @@ for frame_index = 1:length(frame_paths)
 
 
         % --------------------
-        % Simulate psf - replace this whole chunk with reading in an image
+        % Simulate psf
+        % !!! replace this whole chunk with using the image !!!
         % --------------------
         % Not sure what FitPSF() uses to do the fitting. Does it just use the
         % psf.image part of a psf object? Or does it use the other stuff stored in that?
@@ -120,16 +121,68 @@ for frame_index = 1:length(frame_paths)
         % can pass those as our initial estimates for this to use.
         % Oh and because the patches we consider will be centered on those
         % locations, the initial estimate will always be (0,0) by definition.
+        %
+        % pdf.image seems to be a 2x2 array
+        % with the values spanning 0 to like 4000 or so
+        % normalise to 255 perhaps?
+        % I think it's just photon counts. Currently nPhotoons = 1e5
 
-        % Set input parameters
+        % --- Set input parameters ---
+
+        % Image parameters
+        par.nPixels = patch_width; % Changing this here didn't seem to affect it
+        
+        % Fluorophore parameters
         angleInclination = pi * rand;
         angleAzimuth = 2 * pi * rand;
-        par.position = Length([rand rand rand] * 100, 'nm');
         par.dipole = Dipole(angleInclination, angleAzimuth);
+        par.position = Length([0 0 0], 'nm'); % Because initial estimate is centre of patch (check that 0,0 is centre)
+        par.nPhotons = 1e5;
+        par.shotNoise = 0;
+        par.reducedExcitation = 0;
         par.stageDrift = LinearDrift(0.0001, 0.0001, 1, 'nm');%(10, 1, 10, 'nm');
-        par.defocus = Length(-500 + 1000 * rand(), 'nm');
-        % Simulate
+
+        % Microscope parameters
+        par.defocus = Length(0, 'nm'); % Assume no defocus for now
+        par.wavelength = Length(680,'nm');
+        par.astigmatism = 0; % Zernike coefficient (in units of wavelength, i.e. 0.11 for Zernike coefficient 0.11*lambda)
+        par.objectiveNA = 0.7;
+        par.objectiveFocalLength = Length(3,'mm');
+        par.refractiveIndices = [1.33 1.46 1]; % [RI_specimen, RI_intermed, RI_immoil]
+        par.heightIntermediateLayer = Length(0, 'mm');
+        
+        % BFP parameters
+        %psf.phaseMask = @(n) EmptyPhaseMask(n)
+        %psf.attenuation = @(n) Aperture(n)
+        par.nDiscretizationBFP = 129;
+        
+        % Camera parameters
+        par.pixelSize = Length(51.2,'nm');
+        par.pixelSensitivityMask = PixelSensitivity.uniform(9);
+        par.backgroundNoise = 0;
+        % --- end parameters ----
+
+        % Simulate 
         psf = PSF(par);
+
+        % now swap out the psf.image for the patch image created earlier -
+        % no idea if this is right or doing the right thing at all, and I
+        % don't know what it should be normalised to
+        patch_image = patches{blob_index}
+        patch_image = double(patch_image)
+
+        % Normalize the image to the range [0, max(psf.image(:))]
+        patch_min = min(patch_image(:));  % Find the minimum value
+        patch_max = max(patch_image(:));  % Find the maximum value
+        patch_image = (patch_image - patch_min) * (max(psf.image(:)) / (patch_max - patch_min));
+
+        psf.image = patch_image; % but need to normalise it to whatever this expects...
+
+
+        % Here, chop out psf.image and replace it with the patch image
+        % created above
+
+
         % --------------------
 
         % Fitting
@@ -173,5 +226,26 @@ y_estimates = [y_estimates{:}];
 defocus_estimates = [defocus_estimates{:}];
 
 % Make a copy of thunderstorm results table
+copyfile(thunderstorm_results_path, hinterer_results_path);
 
-% Add new localisations to those
+% Add new localisations to it
+tbl = readtable(hinterer_results_path);
+
+% Add localisations if the lengths match
+if length(x_estimates) == height(tbl)
+    tbl.('x_nm_') = tbl.('x_nm_') + x_estimates(:);
+    tbl.('y_nm_') = tbl.('y_nm_') + y_estimates(:);
+else
+    disp('Error: Length of x_estimates does not match the number of rows in the table');
+end
+
+% Renaming because matlab treats the _ and [] weirdly
+tbl.Properties.VariableNames{'x_nm_'} = 'x [nm]';
+tbl.Properties.VariableNames{'y_nm_'} = 'y [nm]';
+tbl.Properties.VariableNames{'sigma_nm_'} = 'sigma [nm]';
+tbl.Properties.VariableNames{'intensity_photon_'} = 'intensity [photon]';
+tbl.Properties.VariableNames{'offset_photon_'} = 'offset [photon]';
+tbl.Properties.VariableNames{'bkgstd_photon_'} = 'bkgstd [photon]';
+tbl.Properties.VariableNames{'uncertainty_nm_'} = 'uncertainty [nm]';
+
+writetable(tbl, hinterer_results_path);
